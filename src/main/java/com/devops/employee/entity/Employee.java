33a3b9cde71b7754b7aaf3cfa2f71b21df6cc8a6
Pkadam422 <pkadam422@gmail.com>
2026-02-22 15:04:52 +0530

    edited entity

--- src/main/java/com/devops/employee/entity/Employee.java
+++ src/main/java/com/devops/employee/entity/Employee.java
@@ -5,13 +5,47 @@ import jakarta.persistence.*;
 @Entity
 public class Employee {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
 
-    private String name;
-    private String department;
-    private Double salary;
+private String name;
+private String department;
+private Double salary;
 
-    // Getters & Setters
+public Employee() {
+}
+
+public Long getId() {
+return id;
+}
+
+public void setId(Long id) {
+this.id = id;
+}
+
+public String getName() {
+return name;
+}
+
+public void setName(String name) {
+this.name = name;
+}
+
+public String getDepartment() {
+return department;
+}
+
+public void setDepartment(String department) {
+this.department = department;
+}
+
+public Double getSalary() {
+return salary;
+}
+
+public void setSalary(Double salary) {
+this.salary = salary;
+}
+
 }
