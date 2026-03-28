f4b4ab0836cd2b33ec88388f110ddde4519763b4
Pkadam422 <pkadam422@gmail.com>
2026-03-28 14:18:51 +0530

    updated /employees

--- src/main/java/com/devops/employee/controller/EmployeeController.java
+++ src/main/java/com/devops/employee/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/employees")
+@RequestMapping("employees")
 public class EmployeeController {
 
     private final EmployeeService service;
